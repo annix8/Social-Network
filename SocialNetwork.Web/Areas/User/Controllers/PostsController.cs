df11731b22917b7b5f47aa40f33f1c8df94ac7b2
annix8 <mario.ivailov@abv.bg>
2017-11-28 23:38:11 +0200

    added posts controller

---
+++ SocialNetwork.Web/Areas/User/Controllers/PostsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using SocialNetwork.Web.Areas.User.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace SocialNetwork.Web.Areas.User.Controllers
+{
+    [Authorize]
+    public class PostsController : UserAreaController
+    {
+        
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(PostModel postModel)
+        {
+            return RedirectToAction(nameof(ProfileController.MyProfile),nameof(ProfileController));
+        }
+    }
+}
