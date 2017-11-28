44ae60cd87aeee30d5388840f8f917d6643d7b43
annix8 <mario.ivailov@abv.bg>
2017-11-28 23:38:26 +0200

    added post model and create post view

---
+++ SocialNetwork.Web/Areas/User/Models/PostModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace SocialNetwork.Web.Areas.User.Models
+{
+    public class PostModel
+    {
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Content { get; set; }
+        public IFormFile Picture { get; set; }
+    }
+}
