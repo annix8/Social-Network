2bfa691cd8055a497f6aea968b271e8e2cddde72
Mario Yordanov <mario.yordanov@pds.nl>
2017-12-18 15:02:16 +0200

    added new test in UserService tests

--- SocialNetwork.Tests/Services/UserServiceTests.cs
+++ SocialNetwork.Tests/Services/UserServiceTests.cs
@@ -52,6 +52,50 @@
                 .Equals("testuser");
         }
 
+        [Fact]
+        public async Task MakeFriendRequestShouldMakeARecordInFriendshipTable()
+        {
+            // Arrange
+            var db = GetDatabase();
+
+            var firstUser = new User
+            {
+                Id = Guid.NewGuid().ToString(),
+                UserName = "testuser",
+                Email = "test@test.com",
+                FirstName = "John",
+                LastName = "Doe"
+            };
+
+            var secondUser = new User
+            {
+                Id = Guid.NewGuid().ToString(),
+                UserName = "testoviuser",
+                Email = "tester@test.com",
+                FirstName = "John",
+                LastName = "Chloe"
+            };
+
+            await db.Users.AddRangeAsync(firstUser, secondUser);
+            await db.SaveChangesAsync();
+
+            var userService = new UserService(db);
+
+            // Act
+            var result = await userService.MakeFriendRequestAsync(firstUser.UserName, secondUser.UserName);
+
+            // Assert
+            result
+                .Should()
+                .Be(true);
+
+            var friendship = await db.Friendships.FirstOrDefaultAsync();
+
+            friendship
+                .Should()
+                .NotBeNull();
+        }
+
         private SocialNetworkDbContext GetDatabase()
         {
             var dbOptions = new DbContextOptionsBuilder<SocialNetworkDbContext>()
