10554f9281c8e8b1f0cfcbee1225e8a9bad4f2f1
annix8 <mario.ivailov@abv.bg>
2017-12-19 20:43:50 +0200

    added more user service tests

--- SocialNetwork.Tests/Services/UserServiceTests.cs
+++ SocialNetwork.Tests/Services/UserServiceTests.cs
@@ -130,5 +130,145 @@
                 .Should()
                 .BeNull();
         }
+
+        [Fact]
+        public async Task AcceptFriendshipShouldReturnFalseIfOneOfTheUsersDoesNotExist()
+        {
+            // Arrange
+            var db = MockManager.GetMockDatabase();
+
+            var firstUser = new User
+            {
+                Id = Guid.NewGuid().ToString(),
+                UserName = "testuser",
+                Email = "test@test.com",
+                FirstName = "John",
+                LastName = "Doe"
+            };
+
+            await db.Users.AddAsync(firstUser);
+            await db.SaveChangesAsync();
+
+            var userService = new UserService(db);
+
+            // Act
+            var result = await userService.AcceptFriendshipAsync(firstUser.UserName, "unexistingUser");
+
+            // Assert
+            result
+                .Should()
+                .Be(false);
+        }
+
+        [Fact]
+        public async Task AcceptFriendshipShouldReturnTrueIfBothUsersExist()
+        {
+            // Arrange
+            var db = MockManager.GetMockDatabase();
+
+            var firstUser = new User
+            {
+                Id = Guid.NewGuid().ToString(),
+                UserName = "testuser",
+                Email = "test@test.com",
+                FirstName = "John",
+                LastName = "Doe"
+            };
+
+            var secondUser = new User
+            {
+                Id = Guid.NewGuid().ToString(),
+                UserName = "testoviuser",
+                Email = "test@email.com",
+                FirstName = "Blq",
+                LastName = "Qlb"
+            };
+
+            await db.Users.AddRangeAsync(firstUser, secondUser);
+
+            await db.SaveChangesAsync();
+
+            var friendship = new Friendship
+            {
+                User = firstUser,
+                Friend = secondUser,
+                FriendshipIssuerId = firstUser.Id
+            };
+
+            await db.Friendships.AddAsync(friendship);
+            await db.SaveChangesAsync();
+
+            var userService = new UserService(db);
+
+            // Act
+            var result = await userService.AcceptFriendshipAsync(firstUser.UserName, secondUser.UserName);
+
+            // Assert
+            result
+                .Should()
+                .Be(true);
+        }
+
+        [Fact]
+        public async Task DeleteAccountShouldReturnTrueIfUserExists()
+        {
+            // Arrange
+            var db = MockManager.GetMockDatabase();
+
+            var firstUser = new User
+            {
+                Id = Guid.NewGuid().ToString(),
+                UserName = "testuser",
+                Email = "test@test.com",
+                FirstName = "John",
+                LastName = "Doe"
+            };
+
+            var secondUser = new User
+            {
+                Id = Guid.NewGuid().ToString(),
+                UserName = "testoviuser",
+                Email = "test@email.com",
+                FirstName = "Blq",
+                LastName = "Qlb"
+            };
+
+            await db.Users.AddRangeAsync(firstUser, secondUser);
+
+            await db.SaveChangesAsync();
+
+            var userService = new UserService(db);
+
+            // Act
+            var result = await userService.DeleteAccountAsync(firstUser.UserName);
+
+            // Assert
+            result
+                .Should()
+                .Be(true);
+
+            var users = await db.Users.ToListAsync();
+
+            users
+                .Should()
+                .HaveCount(1, "Because one of the two users in the db was deleted");
+        }
+
+        [Fact]
+        public async Task DeleteAccountShouldReturnFalseIfUserDoesNotExist()
+        {
+            // Arrange
+            var db = MockManager.GetMockDatabase();
+
+            var userService = new UserService(db);
+
+            // Act
+            var result = await userService.DeleteAccountAsync("unexistingUser");
+
+            // Assert
+            result
+                .Should()
+                .Be(false);
+        }
     }
 }
